3b8f8c145766f0cde5907309e268efcd50d9fca6
gminos <gjosebelarmino@gmail.com>
2025-11-17 22:18:09 +0000

    refactor: unificar el proceso de build del frontend y simplificar la configuración de nginx

--- nginx/entrypoint.sh
+++ nginx/entrypoint.sh
@@ -1,7 +1,9 @@
 #!/bin/sh
-
 set -e
 
+echo "Generando archivo Nginx en /etc/nginx/conf.d/default.conf con DOMAIN=${DOMAIN}..."
 envsubst '${DOMAIN}' </etc/nginx/conf.d/default.conf.template >/etc/nginx/conf.d/default.conf
+echo "Archivo generado correctamente. Contenido final:"
+cat /etc/nginx/conf.d/default.conf
 
 exec "$@"
